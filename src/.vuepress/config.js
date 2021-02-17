adaa3acefadb850b03b6c0a0b3c74116cf6ce98a
Xiao Hanyu <xiaohanyu1988@gmail.com>
2021-02-17 08:56:35 +0800

    Add GA

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -62,5 +62,11 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    [
+      '@vuepress/google-analytics',
+      {
+        'ga': 'UA-49528621-1'
+      }
+    ]
   ]
 }
